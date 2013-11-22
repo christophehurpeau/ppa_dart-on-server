f7da6852f0400b5fb125f745e0981428921273a6
Christophe Hurpeau <christophe@hurpeau.com>
2013-11-23 00:05:22 +0100

    scripts

--- dart-app-new.sh
+++ dart-app-new.sh
@@ -6,11 +6,15 @@ do
 	read -p "Please provide your app name: (eg: my_app) " appName
 done
 
-sudo useradd --home-dir /var/dartapps/$appName -g dart --create-home --shell /bin/bash $appName
+sudo useradd --home-dir /var/dartapps/$appName -g dart --create-home --shell /bin/bash "dart-$appName"
 
 if [ ! -d /var/dartapps/$appName ]; then
 	echo "User creation failed"
 	exit 1;
 fi
 
+read -p "Url of the repository to clone (just press enter if none)" repo
 
+if [ "$repo" != "" ]; then
+	sudo su "dart-$appName" -s "git clone '$repo' '/var/dartapps/$appName/app/'"
+fi
--- dart-app-restart.sh
+++ dart-app-restart.sh
@@ -3,6 +3,13 @@
 appName = ''
 while [ "$appName" = "" ]
 do
-	read -p "Please provide your app name: (eg: my_app) " appName
+	read -p "Please provide your app name: " appName
 done
 
+script = ''
+read -p "Please provide your script in bin/: [server.dart]" script
+if [ "$script" = "" ]; then
+	script = 'server.dart'
+fi
+
+sudo su "dart-$appName" -s "killall dart ; cd '/var/dartapps/$appName/app/' && dart bin/$script"
